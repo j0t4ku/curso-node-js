8e32e5d0f8b6ed76ea0f848ccff7dec73fcb5a66
j0t4ku <j.ismar00@gmail.com>
2023-10-25 20:00:25 -0300

    add cors and fix delete cors

--- clase-3/app.js
+++ clase-3/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const movies = require('./movies.json')
+const cors = require('cors')
 const crypto = require('node:crypto')
 const { validateSchema, validatePartialMovie } = require('./schemas/movies')
 
@@ -15,16 +16,39 @@ const ACEPTED_ORIGINS = [
   'http://production.com'
 ]
 
+app.use(cors({
+  origin: (origin, callback) => {
+    const ACCEPTED_ORIGINS = [
+      'http://localhost:8080',
+      'http://localhost:3000',
+      'http://production.com'
+    ]
+
+    if (ACCEPTED_ORIGINS.includes(origin)) {
+      return callback(null, true)
+    }
+
+    if (!origin) {
+      return callback(null, true)
+    }
+
+    return callback(new Error('Not allowed by CORS'))
+  }
+}))
+
 app.get('/', (req, res) => {
   res.json({ message: 'hola mundo' })
 })
 
 app.get('/movies', (req, res) => {
   const origin = req.header('origin')
+
   if (ACEPTED_ORIGINS.includes(origin) || !origin) {
     res.header('Access-Control-Allow-Origin', '*')
   }
+
   const { genre } = req.query
+
   if (genre) {
     const filterdMovies = movies.filter(movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase()))
     return res.json(filterdMovies)
@@ -84,14 +108,14 @@ app.patch('/movies/:id', (req, res) => {
   return res.json(updateMovie)
 })
 
-app.options('/movies/:id', (req, res) => {
-  const origin = req.header('origin')
-  if (ACEPTED_ORIGINS.includes(origin) || !origin) {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE')
-  }
-  res.send(200)
-})
+// app.options('/movies/:id', (req, res) => {
+//   const origin = req.header('origin')
+//   if (ACEPTED_ORIGINS.includes(origin) || !origin) {
+//     res.header('Access-Control-Allow-Origin', origin)
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE')
+//   }
+//   res.send(200)
+// })
 
 const PORT = process.env.PORT ?? 3000
 
